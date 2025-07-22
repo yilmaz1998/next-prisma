e837751cc4475edb0026707cfbb918db311fb31c
yilmaz1998 <snn.ylmz9804@gmail.com>
2025-07-21 21:34:24 -0400

    User migration

---
+++ next-prisma/prisma/migrations/20250722013323_add_user_model/migration.sql
@@ -0,0 +1,17 @@
+-- AlterTable
+ALTER TABLE "Todo" ADD COLUMN     "userId" TEXT;
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Todo" ADD CONSTRAINT "Todo_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
